028a52e0d739225be9a6eeefede3e2070ab89397
Lofftavelglarn <52246786+Lofftavelglarn@users.noreply.github.com>
2026-02-27 02:45:05 +0700

    Update solution for sigmoid-numpy

--- sigmoid-numpy/sigmoid-numpy.py
+++ sigmoid-numpy/sigmoid-numpy.py
@@ -4,10 +4,5 @@ def sigmoid(x):
     """
     Vectorized sigmoid function.
     """
-    # Write code here
     x = np.asarray(x, dtype=float)
-    if x.all() >= 0:
-        return 1 / (1 + np.exp(-x))
-    
-    else:
-        return np.exp(x) / (1 + np.exp(x))
+    return np.where(x >= 0, 1 / (1 + np.exp(-x)), np.exp(x) / (1 + np.exp(x)))
